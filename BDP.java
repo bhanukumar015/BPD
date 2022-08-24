b069ea266822b645665b25d19db2a3d839363fe3
PavaniSubhasri <pavanisubhasri27.s@gmail.com>
2022-08-24 21:55:45 +0530

    Our taglines

---
+++ BDP.java
@@ -0,0 +1,7 @@
+class BDP {
+    public static void main(String[] args){
+        System.out.println("Pavani is a good girl");
+        System.out.println("Divya is a crazy girl");
+        System.out.println("We don't know about bhanu");
+    }
+}
